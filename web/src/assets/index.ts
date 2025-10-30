3c30d62eda974f6cb8f418f534a624c6d56aaa0d
FliprInternAlfaiz <mrangrez@flipr.ai>
2025-10-30 18:09:20 +0530

    fix icon

--- web/src/assets/index.ts
+++ web/src/assets/index.ts
@@ -1,6 +1,6 @@
 import Apple_logo from "./Apple_logo.png";
 import Home_Page from "./Home_Page.jpeg";
-import badge_icon from "./BADGE_ICON.png";
+import badge_icon from "./Badge_icon.png";
 import ad1 from "./ad1.jpeg";
 import ad2 from "./ad2.jpeg";
 export const IMAGES = {
